ebb9c91db1048cf0b94a9d79cc13071fdea1817d
AnjaMis <bgp916@myy.haaga-helia.fi>
2021-12-15 01:42:14 +0200

    stats

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { PureComponent, useState, useEffect } from 'react'
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
 
 function Statistics() {
+  const [trainings, setTrainings] = useState([])
+
+  useEffect(() => {
+    fetchTrainings()
+  }, [])
+
+  const fetchTrainings = () => {
+    fetch('https://customerrest.herokuapp.com/api/trainings')
+      .then((response) => response.json())
+      .then((data) => setTrainings(data.content))
+      .catch((err) => console.error(err))
+  }
+
+  const data = [
+    {
+      name: 'trainings.activity',
+      duration: 'trainings.duration',
+    },
+  ]
   return (
     <div>
-      <h1>Statistics </h1>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="duration" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   )
 }
